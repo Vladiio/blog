d993935c3c510d2bfa8648521f17c36dd27f8a9b
Vlad <unix.vlad@yandex.ru>
2017-02-27 22:14:16 +0200

    add view for handling the email post form

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 
+from .forms import EmailPostForm
 from .models import Post
 
 
@@ -39,3 +40,31 @@ class PostListView(ListView):
     context_object_name = 'posts'
     paginate_by = 3
     template_name = 'blog/post/list.html'
+
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status='published')
+
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+
+        if form.is_valid():
+
+            cd = form.cleaned_data
+            # send email
+
+    else:
+        form = EmailPostForm()
+
+    return render(request, 'blog/post/share.html', {'form': form,
+                                                    'post': post})
+
+
+
+
+
+
+
+
+
+
