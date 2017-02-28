2020b56537139b75974558d636676ac0beef7a5d
Vlad <unix.vlad@yandex.ru>
2017-02-28 19:00:45 +0200

    retrieving posts by similarity

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from django.core.mail import send_mail
+from django.db.models import Count
 
 from taggit.models import Tag
 
@@ -53,12 +54,19 @@ def post_detail(request, year, month, day, post):
     else:
         comment_form = CommentForm()
 
+    # List of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids)\
+        .exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
+        .order_by('-same_tags', '-publish')[:4]
     return render(request,
                   'blog/post/detail.html',
                   {'post': post,
                    'comments': comments,
                    'comment_form': comment_form,
-                   'new_comment': new_comment})
+                   'new_comment': new_comment,
+                   'similar_posts': similar_posts})
 
 
 class PostListView(ListView):
@@ -80,13 +88,10 @@ def post_share(request, post_id):
             cd = form.cleaned_data
             # send email
             post_url = request.build_absolute_uri(post.get_absolute_url())
-
             subject = '{} ({}) recommends you reading "{}"'.\
                 format(cd['name'], cd['email'], post.title)
-
             message = 'Read "{}" at {}\n\n{}\'s comments: {}'.\
                 format(post.title, post_url, cd['name'], cd['comments'])
-
             send_mail(subject, message, 'admin.domain.com', [cd['to']])
             sent = True
     else:
